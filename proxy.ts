334dc5d189695fbabf3a853ce7d58a10471af460
lameduc344 <famusimage@gmail.com>
2026-08-06 20:37:50 -0400

    Add permanent deployment cache refresh safeguards

---
+++ proxy.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export function proxy() {
+  const response = NextResponse.next();
+
+  response.headers.set(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, max-age=0",
+  );
+  response.headers.set("Pragma", "no-cache");
+  response.headers.set("Expires", "0");
+
+  return response;
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\..*).*)",
+  ],
+};
